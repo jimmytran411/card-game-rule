559baf2b4665fe0d089e0624ddf9ca279f7f6136
An Tran <jimmytran411@gmail.com>
2021-07-22 14:37:50 +0300

    fix table of content bug
    fix bug when scrolling at the bottom will keep changing between scrolling and not

--- src/Components/Generic/NestedNav.tsx
+++ src/Components/Generic/NestedNav.tsx
@@ -1,20 +1,18 @@
-import React from 'react';
-import {  ListItem, ListItemText, makeStyles, Theme } from '@material-ui/core';
-import { NavLink, useRouteMatch } from 'react-router-dom';
-import { v4 as uuidV4 } from 'uuid';
-import AutoSizer from 'react-virtualized-auto-sizer';
-import { FixedSizeList } from 'react-window';
+import React, { useRef } from "react";
+import { List, ListItem, ListItemText, makeStyles, Theme } from "@material-ui/core";
+import { NavLink, useRouteMatch } from "react-router-dom";
+import { v4 as uuidV4 } from "uuid";
+import AutoSizer from "react-virtualized-auto-sizer";
+import { FixedSizeList, ListOnScrollProps } from "react-window";
 
 const useStyles = makeStyles((theme: Theme) => ({
   defaultNav: {
-    textDecoration: 'none',
-    color: 'inherit',
-   
+    textDecoration: "none",
+    color: "inherit",
   },
   defaultActiveNav: {
-    "& div" : {backgroundColor: theme.palette.action.hover,}
+    "& div": { backgroundColor: theme.palette.action.hover },
   },
- 
 }));
 
 export interface NavItem {
@@ -31,33 +29,61 @@ interface NestedNavProps {
 
 export const NestedNav: React.FC<NestedNavProps> = ({ navList }) => {
   const { url } = useRouteMatch();
-  const { defaultNav, defaultActiveNav,  } = useStyles();
+  const { defaultNav, defaultActiveNav } = useStyles();
 
+  const [isBottom, setIsBottom] = React.useState(false);
+  const outerRef = useRef<{ offsetHeight: number; scrollHeight: number } | null>();
+
+  const onScroll = ({ scrollOffset, scrollUpdateWasRequested }: ListOnScrollProps) => {
+    if (scrollUpdateWasRequested === false) {
+      setIsBottom(false);
+    }
+
+    if (!outerRef.current) {
+      return;
+    }
+
+    if (scrollOffset + outerRef.current.offsetHeight === outerRef.current.scrollHeight) {
+      setIsBottom(true);
+    }
+  };
   return (
     <AutoSizer>
       {({ width, height }) => (
-        <FixedSizeList width={width} height={height} itemCount={navList.length} itemSize={40} itemData={navList}>
-          {({ index, data, style }) => {
-            const { path, content, contentClassName, activeClassName, navClassName } = data[index];
+        <FixedSizeList
+          width={width}
+          height={height}
+          itemCount={navList.length}
+          itemSize={40}
+          itemData={navList}
+          onScroll={onScroll}
+          useIsScrolling={!isBottom}
+          outerRef={outerRef}
+        >
+          {({ index, data, style, isScrolling }) => {
+            const { path, content, contentClassName, activeClassName, navClassName } =
+              data[index];
             return (
-             
-                <NavLink 
-                style={style}
-                key={uuidV4()}
-                activeClassName={activeClassName ? activeClassName : defaultActiveNav}
-                className={navClassName ? navClassName : defaultNav}
-                to={`${url}/${path}`}
-              >
-                <ListItem button >
-                  {typeof content === 'string' ? (
-                    <ListItemText className={contentClassName} primary={content} />
-                  ) : (
-                    content()
-                  )}
-                </ListItem>              
-              </NavLink>
-             
-              
+              <List style={style}>
+                {isScrolling ? (
+                  "Loading ..."
+                ) : (
+                  <NavLink
+                    key={uuidV4()}
+                    activeClassName={activeClassName ? activeClassName : defaultActiveNav}
+                    className={navClassName ? navClassName : defaultNav}
+                    to={`${url}/${path}`}
+                  >
+                    <ListItem button>
+                      {typeof content === "string" ? (
+                        <ListItemText className={contentClassName} primary={content} />
+                      ) : (
+                        content()
+                      )}
+                    </ListItem>
+                  </NavLink>
+                )}
+              </List>
             );
           }}
         </FixedSizeList>
